94ad357157262b0a16f7e6420c01795ecd84551a
Blieque Mariguan <himself@blieque.co.uk>
2018-01-21 01:02:54 +0000

    added colour schemes, changed tiles, added fonts
    - Added colour scheme generation from the secondary page, and tweaked it
      a fair amount to make it suit better.
    - Removed a few of the tile patterns, and added some simpler ones in
      their place.
    - Added webfont versions of CMU Serif and Overpass Mono, and removed
      TrueType versions of Droid Sans.
    - Added font licenses to the license file, and reflowed some paragraphs
      to 80 columns because I'm a hopeless pedant.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,7 +2,9 @@
 
 const tiles = [
     // "X"
-    [[0,12], [1,11], [2,10], [3,9], [4,8], [5,7], [6]],
+    [[0,12], [1,11], [2,10], [3,9], [4,8], [5], [6]],
+    // "X" alternative
+    [[0,12], [1,11], [2,10], [3,9], [4,8], [7], [6]],
     // "/"
     [[12], [11], [10], [9], [8], [7], [6]],
     // "\"
@@ -11,12 +13,18 @@ const tiles = [
     [[12], [11], [10], [9], [8,4], [7,5], [6]],
     // "\" with small cross
     [[0], [1], [2], [3], [4,8], [5,7], [6]],
+    // "/" with small cross alternative
+    [[12], [11], [10], [9], [8,4], [5], [6]],
+    // "\" with small cross alternative
+    [[0], [1], [2], [3], [4,8], [7], [6]],
+    /* The tile patterns were removed for feeling a bit too cluttered.
     // diamond
     [[0,12], [1,11], [2,6,10], [3,5,7,9], [4,8], [3,9], [2,10]],
     // "X" with small gaps
     [[0,12], [1,11], [2,10], [], [4,8], [5,7], [6]],
     // "X" without centre
     [[0,12], [1,11], [2,10], [3,9], [], [], []],
+    */
 ].map((tilePrototype) => {
     /* Converts each array of array of numbers above to an array of array of
      * booleans, which avoids the need to call `.includes()' for every pixel we
@@ -30,6 +38,7 @@ const tiles = [
         }
         tile.push(row);
     }
+
     /* Goes back up and copies rows 1 to 5, mirroring and shifting them one
      * pixel right, and appends them to the tile map. This halves the required
      * size of the big number arrays above.
@@ -46,10 +55,10 @@ const tiles = [
 
 const generateDistribution = function generateDistribution() {
     /* I want the highest proportion of the tiles drawn to be blank. This line
-     * ensures that 36%-81% of the tiles will be blank, with a slight bias
+     * ensures that 49%-81% of the tiles will be blank, with a slight bias
      * towards the lower end.
      */
-    const spacePortion = (0.6 + Math.random() * 0.3) ** 2;
+    const spacePortion = (0.7 + Math.random() * 0.2) ** 2;
     const pickPortion = 1 - spacePortion;
 
     /* This block picks 2+ tile patterns from the `tiles' array, and generates
@@ -79,20 +88,114 @@ const generateDistribution = function generateDistribution() {
     return {
         picks,
         pickPoints,
-        highestIndex: picks.length - 1,
+        pickCount: picks.length,
     };
 };
 
-const resize = function resize() {
-    el.canvas.width = window.innerWidth;
-    el.canvas.height = window.innerHeight;
-    draw(generateDistribution());
+const generateScheme = function generateScheme() {
+    /* Background lightness has a pretty strong bias towards 50%, making very
+     * light and very dark backgrounds a lot rarely than they'd otherwise be.
+     */
+    //const backgroundLightness = ((Math.random() - 0.5) * 1.58) ** 3 + 0.5;
+    const x = Math.random();
+    const backgroundLightness = (1.1 * (x - 0.5)) ** 3 + 0.1 * x + 0.17;
+    let lightnessRange;
+    let lightnessOffset;
+
+    /* A lightness range is generated for the swatches, so that all values are
+     * darker than the background.
+     */
+    lightnessRange = (1 - backgroundLightness) * 0.8;
+    lightnessOffset = backgroundLightness + lightnessRange * 0.25;
+
+    //const lightnessScale = 0.3 + 0.5 * Math.random();
+    const lightnessScale = 0.8;
+    lightnessOffset +=
+        lightnessRange * (1 - lightnessScale) * Math.random();
+    lightnessRange *= lightnessScale;
+
+    /* A random base hue is generated, and used for the background. A hue range
+     * is generated for the swatches, and is offset by some amount. This allows
+     * for some schemes with a similar-hue background, and others with more
+     * complementary colours.
+     */
+    const baseHue = Math.random() * 360;
+    const hueRange = 30 + 90 * Math.random();
+    const hueOffsetMax = 90;
+    const hueOffset =
+        -hueOffsetMax + hueOffsetMax * 2 * Math.random() - (hueRange / 2);
+
+    /* Saturation range, with a bias towards the upper end. */
+    const saturation = 0.4 + 0.35 * (1 - (1 - Math.random()) ** 1.5);
+
+    /* Create the array to hold HSL colour arrays. */
+    const scheme = [];
+
+    /* Generate and add the swatch colours to the array. Hues are evenly
+     * distributed within the previously allocated range, saturation is uniform
+     * for all swatches and generated within a range, and lightness is generated
+     * per-swatch within a range.
+     */
+    const hueStep = hueRange / 7;
+    for (let i = 0; i < 8; i++) {
+        const hueRaw = baseHue + hueOffset + hueStep * i;
+        const hue = Math.round((hueRaw + 360) % 360);
+        const lightness = lightnessOffset + lightnessRange * Math.random();
+        scheme.push([hue, saturation, lightness]);
+    }
+
+    /* Add the background colour last, and add a getter function to give the
+     * colour an alias.
+     */
+    scheme.push([baseHue, saturation * 0.5, backgroundLightness]);
+    Object.defineProperty(scheme, 'background', { get(){
+        return scheme[scheme.length - 1];
+    } });
+
+    return scheme;
 };
 
-const draw = function draw(distribution) {
+/* HSL to RGB hex converter, based on an algorithm available on Wikipedia.
+ */
+const hslToHex = function hslToHex(hue, saturation, lightness) {
+    const chroma = (1 - Math.abs(2 * lightness - 1)) * saturation;
+    const hueDivision = hue / 60;
+    const x = chroma * (1 - Math.abs(hueDivision % 2 - 1));
+
+    /* Performs the mapping from the divided hue value to one of six possible
+     * initial RGB arrays
+     */
+    let hueDivisionFloor = Math.floor(hueDivision);
+    let rgb;
+    if (hueDivisionFloor % 2 === 0) {
+        rgb = [chroma, x, 0];
+    } else {
+        hueDivisionFloor--;
+        rgb = [x, chroma, 0];
+    }
+    for (let i = 0; i < hueDivisionFloor; i += 2) {
+        rgb.push(rgb.shift());
+    }
+
+    /* Adjusts the initial RGB array values by a calculated amount to match
+     * lightness with the HSL colour.
+     */
+    const m = lightness - (chroma / 2);
+    for (let i = 0; i < 3; i++) {
+        rgb[i] = Math.floor((rgb[i] + m) * 255);
+    }
+
+    /* Convert the RGB array to a hex string. */
+    return '#' + rgb.map(v => v.toString(16).padStart(2, '0')).join('');
+};
+
+/* Draw a new random pattern in the canvas, using a provided distribution and
+ * colour scheme.
+ */
+const draw = function draw(distribution, scheme) {
     const {width, height} = el.canvas;
 
-    ctx.fillStyle = '#000';
+    ctx.fillStyle = scheme.background;
     ctx.fillRect(0, 0, width, height);
 
     ctx.fillStyle = '#fff';
@@ -108,6 +211,9 @@ const draw = function draw(distribution) {
             });
 
             if (tile) {
+                const colourIndex = Math.floor(
+                    distribution.pickCount * (cellEntropy * 10000 % 1));
+                ctx.fillStyle = scheme[colourIndex];
                 for (let py = 0; py < 13; py++) {
                     for (let px = 0; px < 13; px++) {
                         if (tile[py][px]) {
@@ -116,17 +222,40 @@ const draw = function draw(distribution) {
                     }
                 }
             }
-
-            /* Draws lines in 0.05% of cells.
-            if (Math.random() > 0.995) {
-                for (let i = 0; i < 10; i++) {
-                    ctx.fillRect(x + i, y + (i * 2), 1, 2);
-                }
-            }
-            */
         }
     }
 };
+
+/* Adjust the size of the canvas to the size of the screen, and call the
+ * generator function to refresh the pattern.
+ */
+const resize = function resize() {
+    el.canvas.width = window.innerWidth;
+    el.canvas.height = window.innerHeight;
+    updatePattern();
+};
+
+/* Optionally generates a new tile distribution and colour scheme, then calls
+ * `draw()' to draw a pattern on the canvas. If a new distribution and scheme
+ * are not requested, the last one used will be re-used.
+ */
+const updatePattern = (() => {
+    let distribution;
+    let scheme;
+
+    return (newEntropy) => {
+        if (newEntropy || !distribution) {
+            distribution = generateDistribution();
+            scheme = generateScheme();
+            for (let i = 0; i < 9; i++) {
+                scheme[i] = hslToHex(...scheme[i]);
+            }
+        }
+        draw(distribution, scheme);
+    };
+})();
+
+// BOOTSTRAPPY STUFF
 
 const el = {};
 el.canvas = document.querySelector('canvas');
@@ -135,4 +264,7 @@ const ctx = el.canvas.getContext('2d');
 const pixelRatio = Math.round(window.devicePixelRatio + 0.5);
 
 window.addEventListener('resize', resize);
-resize();
+resize(); // Will also call `updatePattern()'.
+window.addEventListener('click', () => {
+    updatePattern(true);
+});
